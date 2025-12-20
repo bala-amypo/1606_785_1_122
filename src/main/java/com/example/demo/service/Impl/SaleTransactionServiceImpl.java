bf855154bc3908d1de4354b6a60473185e70187b
bala-amypo <bala@amypo.in>
2025-12-20 05:22:11 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/SaleTransactionServiceImpl.java
@@ -0,0 +1,55 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.DiscountCode;
+import com.example.demo.model.SaleTransaction;
+import com.example.demo.repository.DiscountCodeRepository;
+import com.example.demo.repository.SaleTransactionRepository;
+import com.example.demo.service.SaleTransactionService;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class SaleTransactionServiceImpl implements SaleTransactionService {
+
+    private final SaleTransactionRepository saleTransactionRepository;
+    private final DiscountCodeRepository discountCodeRepository;
+
+    public SaleTransactionServiceImpl(
+            SaleTransactionRepository saleTransactionRepository,
+            DiscountCodeRepository discountCodeRepository) {
+        this.saleTransactionRepository = saleTransactionRepository;
+        this.discountCodeRepository = discountCodeRepository;
+    }
+
+    @Override
+    public SaleTransaction createSale(SaleTransaction transaction) {
+        if (transaction.getTransactionAmount() == null
+                || transaction.getTransactionAmount().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Transaction amount must be > 0");
+        }
+
+        Long codeId = transaction.getDiscountCode().getId();
+
+        DiscountCode code = discountCodeRepository.findById(codeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Discount code not found"));
+
+        transaction.setDiscountCode(code);
+
+        return saleTransactionRepository.save(transaction);
+    }
+
+    @Override
+    public List<SaleTransaction> getSalesForCode(Long discountCodeId) {
+        return saleTransactionRepository.findByDiscountCodeId(discountCodeId);
+    }
+
+    @Override
+    public List<SaleTransaction> getSalesForInfluencer(Long influencerId) {
+        return saleTransactionRepository.findByDiscountCodeInfluencerId(influencerId);
+    }
+
+    @Override
+    public List<SaleTransaction> getSalesForCampaign(Long campaignId) {
+        return saleTransactionRepository.findByDiscountCodeCampaignId(campaignId);
+    }
+}
