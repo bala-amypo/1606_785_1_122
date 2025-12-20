6229f20dffe915cf9d4e8d56477cdd7cc2fa4757
bala-amypo <bala@amypo.in>
2025-12-20 14:00:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/CampaignController.java
+++ src/main/java/com/example/demo/controller/CampaignController.java
@@ -1,9 +1,4 @@
-package com.example.demo.controller;
-
 import com.example.demo.model.Campaign;
-import com.example.demo.service.CampaignService;
-
-import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +7,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/campaigns")
