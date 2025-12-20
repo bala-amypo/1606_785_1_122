a102f386bc2a87d7060795e50ecb9f7331fb4ae7
bala-amypo <bala@amypo.in>
2025-12-20 09:05:40 +0000

    User commit

--- src/main/java/com/example/demo/controller/DiscountCodeController.java
+++ src/main/java/com/example/demo/controller/DiscountCodeController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 @RequestMapping("/discount-codes")
