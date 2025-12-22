6063a64e835b55a34568cc1f5da5940f43a52ccf
bala-amypo <bala@amypo.in>
2025-12-22 03:09:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/RoiReportController.java
+++ src/main/java/com/example/demo/controller/RoiReportController.java
@@ -3,7 +3,13 @@ package com.example.demo.controller;
 import com.example.demo.model.RoiReport;
 import com.example.demo.service.RoiService;
 import java.util.List;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/roi")
